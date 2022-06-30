77a3e3e5bd58fd8417e48e35783783d3ec48d40d
junoade <ajchoi0928@gmail.com>
2022-06-30 21:02:50 +0900

    solve : 4.6 나눗셈 연산없이  x / y 계산하기 - 나눗셈 연산자 없이 덧셈, 뺄셈, 그리고 시프트 연산자로 나눗셈 연산을 구현하였다. - 계산량을 줄이기 위해, 나누어 떨어지는 가장 큰 값 2^k * y 의미와 이를 x에서 뺼셈하는 의미를 파악하였다. > 예를 들어 2^31-1 / 1 를 한다면, > (2^31-1) -1 -1 -1 ... -1 >= 1 까지 수행해야한다. 시간복잡도를 줄일 필요가 있다. > 이를 위해 나누는 수, divisor를 최대로 만든다. > 위의 경우, 1 << 32 를 하고, x와가 큰지, y_max가 큰지 비교한다.

--- epi/ch4_primitives/PrimitiveDivide.java
+++ epi/ch4_primitives/PrimitiveDivide.java
@@ -1,18 +1,44 @@
 package epi.ch4_primitives;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class PrimitiveDivide {
-  @EpiTest(testDataFile = "primitive_divide.tsv")
-  public static int divide(int x, int y) {
-    // TODO - you fill in here.
-    return 0;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "PrimitiveDivide.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+/**
+ * <title>4.6 x/y 계산하기</title>
+ *
+ * @topic 양의 정수 x와 y를 나눈 몫을, 덧셈, 뺼셈 그리고 시프트 연산만을 이용해 구현해보자
+ * @idea (x - y) <= x 일 때 뺼셈을 계속 반복하여 수행한다.
+ * @problem 단순히 y를 계속 빼면 필요한 계산량이 많다. 나누어 떨어지는 가장 큰 값 y로 바꾸어서 x와 뺄셈을 하고 반복을 한다면?
+ * @ref 노션_망각주기_비트연산정리
+ */
+public class PrimitiveDivide {
+    @EpiTest(testDataFile = "primitive_divide.tsv")
+    public static int divide(int x, int y) {
+        // TODO - you fill in here.
+        int k = 32;
+        int quotient = 0;
+        /* overflow 방지 */
+        long y_max = y << k; // y* 2^32
+        while (x >= y) {
+            /* y*2^k <= x 를 만족하는 k를 계산 */
+            while(y_max > x){
+                y_max >>>= 1;
+                --k;
+            }
+            /* 뺼셈 수행, 몫에 값 추가 */
+            quotient += 1 << k;
+            x -= y_max;
+        }
+
+        return quotient;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "PrimitiveDivide.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
